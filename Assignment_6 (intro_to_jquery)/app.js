e72722245cbba727d55c29a138ea6cd764b2395a
A-k-sharma <47344237+A-k-sharma@users.noreply.github.com>
2019-02-15 14:33:33 +0530

    Add files via upload

--- Assignment_6 (intro_to_jquery)/app.js
+++ Assignment_6 (intro_to_jquery)/app.js
@@ -114,3 +114,54 @@ $(document).ready(function()
 	    });
 	})
 });
+
+
+$(function(){
+$('select').on('change',function()
+{
+$("#main-2").append(this.value); 
+});
+});	
+
+
+$(function(){
+$('ul#open').hide();
+$(".dropdown").hover(function() {
+$('ul#open').show();
+},
+function() {
+$('ul#open').hide();
+});
+});	
+
+
+
+$(function(){
+$("#parent").click(function(e){
+$("#parent").append("from click function parent");
+});
+$("#child").click(function(e){
+$("#child").append("from click function child");
+e.stopPropagation();
+});
+});	
+
+
+
+
+$(document).ready(function () 
+{
+	var imageArray = ["https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+	"https://ak.picdn.net/assets/cms/14c40a5a7a7f50788cf9e346071ab49ad1201b12-17d705f0349ed08f5387b39c7644054a59affe52-LOHP_vector_module_shutterstock_307324316-min.jpg",
+	"https://i.pinimg.com/originals/5a/e5/8f/5ae58f5036997cfd4636917403c3c951.jpg"]
+	$("#slider").hover(function () 
+	{
+		var image = $("#slide-img");
+		image.animate(500, function () 
+		{
+			var random = imageArray[Math.floor(Math.random() * imageArray.length)];
+			image.attr("src", random);
+			$("#slider").html(image);
+		})
+	})
+});
